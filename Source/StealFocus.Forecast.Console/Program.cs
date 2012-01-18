96825c1cb1dee5b1ce31c4883fa5106fcf7f0f12
Callum Hibbert <callum.hibbert@gmail.com>
2012-01-18 22:53:46 +0000

    Added console project.

---
+++ Source/StealFocus.Forecast.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StealFocus.Forecast.Console
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                System.Console.WriteLine(arg);
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Source/StealFocus.Forecast.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Beazley">
+//   Copyright Beazley. All rights reserved.
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("StealFocus.Forecast.Console")]
+[assembly: AssemblyTitle("StealFocus.Forecast.Console")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("3091b23e-c2c6-4d6c-a0ae-2d469492910f")]
+[assembly: NeutralResourcesLanguage("en-GB")]
+[assembly: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")] // Assembly is delay signed.
+[assembly: SuppressMessage("Microsoft.Design", "CA1016:MarkAssembliesWithAssemblyVersion")] // Development version is "0.0.0.0"
+[assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")] // "AssemblyInformationalVersion" is a string
