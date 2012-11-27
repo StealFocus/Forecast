9268cf0a9f0a5e3f8ceea9ae81eb13e40cc75ea7
Callum Hibbert <callum.hibbert@gmail.com>
2012-11-27 23:22:11 +0000

    Relying on TopShelf command line args for username and password (rather than prompting). Plus warning user if args missing and advice on access to certificate store.

--- Source/StealFocus.Forecast.Windows.Service/Program.cs
+++ Source/StealFocus.Forecast.Windows.Service/Program.cs
@@ -14,32 +14,60 @@
 
         internal static void Main(string[] args)
         {
-            if (args != null && args.Length > 1)
-            {
-                const string WarningMessage = "Additional arguments will be ignored.";
-                Logger.Warn(WarningMessage);
-                Logger.Warn(string.Empty);
-            }
+            CheckArguments(args);
+            ConfigureWindowsService();
+        }
 
+        private static void CheckArguments(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                bool installArgumentFound = false;
+                bool usernameArgumentFound = false;
+                foreach (string arg in args)
+                {
+                    if (arg == "install")
+                    {
+                        installArgumentFound = true;
+                    }
+
+                    if (arg.StartsWith("-username:", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        usernameArgumentFound = true;
+                    }
+                }
+
+                if (installArgumentFound)
+                {
+                    if (!usernameArgumentFound)
+                    {
+                        Logger.Warn("Without supplying the username and password arguments, this service will install as 'Local System'.");
+                    }
+
+                    Logger.Warn("The account under which this service will run must have certificates matching the thumbprints of those in the configuration.");
+                }
+            }
+        }
+
+        private static void ConfigureWindowsService()
+        {
             HostFactory.Run(hostConfigurator =>
             {
                 hostConfigurator.Service<Forecast.Host>(serviceConfigurator =>
                 {
                     serviceConfigurator.ConstructUsing(name => new Forecast.Host());
                     serviceConfigurator.WhenStarted(host =>
-                        {
-                            string title = string.Format(CultureInfo.CurrentCulture, "StealFocus Forecast Windows Service. Version {0}", typeof(Program).Assembly.GetName().Version);
-                            Forecast.Host.OutputVersionAndCopyrightMessage(title);
-                            host.Start();
-                        });
+                    {
+                        string title = string.Format(CultureInfo.CurrentCulture, "StealFocus Forecast Windows Service. Version {0}", typeof(Program).Assembly.GetName().Version);
+                        Forecast.Host.OutputVersionAndCopyrightMessage(title);
+                        host.Start();
+                    });
                     serviceConfigurator.WhenStopped(host =>
-                        {
-                            host.Stop();
-                            host.WaitForWorkersToStop();
-                        });
+                    {
+                        host.Stop();
+                        host.WaitForWorkersToStop();
+                    });
                 });
-                hostConfigurator.RunAsPrompt(); // Prompt for the service credentials when installing the service.
-                hostConfigurator.SetDescription("The StealFocus Forecast Windows Service.");
                 Version version = typeof(Program).Assembly.GetName().Version;
                 string displayName = string.Format(CultureInfo.CurrentCulture, "StealFocus Forecast Windows Service v{0}", version);
                 hostConfigurator.SetDisplayName(displayName);
